71002ecc78af4330184f8441c3ce4f71f0033805
Ucuki <radidit123@gmail.com>
2024-05-27 09:09:26 +0800

    Add files via upload

---
+++ phising/main.py
@@ -0,0 +1,9 @@
+import time
+from colorama import init, Fore, Back, Style
+
+def run():
+    print(f'''
+          @#######@@ @#@#@#$ @#@####### @##@#@######@#@@@#@##@#@
+          #####@##@#@##@#{Fore.LIGHTBLUE_EX}_W_E_L_C_O_M_E_{Style.NORMAL}#####@##@#@##@#
+
+run()
